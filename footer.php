d1f3d0fc81a474f0b6ed96e5788a66beff3eaf79
yamaguchi371 <telework-takefu6@outlook.jp>
2020-11-13 15:01:02 +0900

    11/13

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <footer id="footer" class="footer flex-box">
     <div class="flex-item">
-        <p>©2016－2020　<?php bloginfo('name') ?><br>〒914-0131福井県敦賀市公文名51-10-1<br>TEL&nbsp;0770-47-6526&nbsp;&nbsp; FAX&nbsp;0770-47-6527</p>
+        <p>©2016－2020　<?php bloginfo('name') ?><br>〒914-0131福井県敦賀市公文名51-10-1<br>TEL<span></span>0770-47-6526FAX<span></span>0770-47-6527</p>
     </div>
 </footer>
 </div><!-- wrapper end -->
